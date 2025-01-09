f17fc20bcf0ec1f0b0566a777186ab6668f6a909
MrPotato9048 <kting9048@gmail.com>
2025-01-09 17:20:00 +0800

    Enhance TTS functionality with audio file generation and playback support

--- tts.py
+++ tts.py
@@ -1,27 +1,11 @@
 import azure.cognitiveservices.speech as speechsdk
-import os
+import os, uuid
 
 key, region = os.getenv('SPEECH_KEY'), os.getenv('SPEECH_REGION')
 audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
 speech_config = speechsdk.SpeechConfig(subscription=key, region=region)
 
-# somehow can stop audio playing
-
-"""isSpeaking = False
-
-def synthesis_started(event):
-    global isSpeaking
-    isSpeaking = True
-
-def synthesis_completed(event):
-    global isSpeaking
-    isSpeaking = False
-
-speech_synthesizer.synthesis_started.connect(synthesis_started)
-speech_synthesizer.synthesis_completed.connect(synthesis_completed)"""
-
 def speak_text(text, lang):
-    # global isSpeaking
     match lang:
         case "en":
             speech_config.speech_synthesis_voice_name='en-US-AvaMultilingualNeural'
@@ -35,14 +19,17 @@ def speak_text(text, lang):
     speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
     speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
 
-    """# save the speech audio to computer as wav file
-    stream = speechsdk.AudioDataStream(speech_synthesis_result)
-    stream.save_to_wav_file("/output/tts.wav)"""
-
     print("Running TTS...")
 
     if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
         print("Speech synthesized for text [{}]".format(text))
+        output_dir = os.path.join(os.getcwd(), 'static', 'output')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        audio_filename = os.path.join(output_dir, f"{uuid.uuid4()}.wav")
+        stream = speechsdk.AudioDataStream(speech_synthesis_result)
+        stream.save_to_wav_file(audio_filename)
+        return audio_filename
     elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
         cancellation_details = speech_synthesis_result.cancellation_details
         print("Speech synthesis canceled: {}".format(cancellation_details.reason))
@@ -50,11 +37,4 @@ def speak_text(text, lang):
             if cancellation_details.error_details:
                 print("Error details: {}".format(cancellation_details.error_details))
                 print("Did you set the speech resource key and region values?")
-
-"""def stop():
-    global isSpeaking
-    if isSpeaking:
-        print("Stopping TTS...")
-        speech_synthesizer.stop_speaking_async()
-        print("TTS stopped successfully")
-        isSpeaking = False"""
+        return None
